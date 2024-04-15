4ec4b079e7df3bde6daf44567b1469570c15b9cb
Szymon Stasiak <01181222@pw.edu.pl>
2024-04-15 16:03:24 +0000

    Revert "Created Data Base Created User table and implemented Auth"

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,9 @@
-import Auth.User.UserService;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.Persistence;
-import Auth.User.User;
-import Auth.User.UserRepository;
+import DataBase.H2DataBase;
 
 public class Main {
     public static void main(String[] args) {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("my-persistence-unit");
-        EntityManager em = emf.createEntityManager();
-        //TEST AND PROVE THAT AUTHENTICATION WORKS
-        //TO DELETE LATTER
-        UserService userService = new UserService(em);
-        User user = new User("user56", "u56@gmail.com" , "password");
-        userService.saveUser(user);
-        User user1 = userService.getUserByLogin("user56", "password");
-        System.out.println(user1.getLogin() + " " + user1.getEmail() + " " + user1.getPassword() + " " + user1.getSpentTime());
+        H2DataBase.startServer();
+        System.out.println("Hello world!");
+        H2DataBase.stopServer();
     }
 }
