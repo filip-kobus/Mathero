573efb2e8dda88866fd1cba9dff9ec1f62f10a8e
filipkobus <kobusfilip@outlook.com>
2024-05-21 09:41:55 +0200

    stworzenie klasy ExerciseStack

--- src/main/java/Gui/ExercisesStack.java
+++ src/main/java/Gui/ExercisesStack.java
@@ -4,29 +4,30 @@ import java.util.ArrayList;
 
 public class ExercisesStack {
     private final ArrayList<ArrayList<String>> allExercise;
-    private ArrayList<String> exercise;
     private int currIndeks;
 
     public ExercisesStack() {
         this.allExercise = new ArrayList<>();
-        this.exercise = new ArrayList<>();
         this.currIndeks = -1;
     }
 
     public ArrayList<String> getPrevious() {
-        if(currIndeks <= 0) return this.allExercise.get(this.currIndeks);
-        this.currIndeks--;
-        exercise = this.allExercise.get(this.currIndeks);
-        return exercise;
+        if (currIndeks <= 0) {
+            return allExercise.get(currIndeks);
+        }
+        currIndeks--;
+        return allExercise.get(currIndeks);
     }
 
     public void push(ArrayList<String> element) {
-        this.exercise = element;
-        this.allExercise.add(this.exercise);
-        this.currIndeks++;
+        allExercise.add(element);
+        currIndeks++;
     }
 
     public ArrayList<String> getExercise() {
-        return this.allExercise.get(this.currIndeks);
+        if (currIndeks >= 0) {
+            return allExercise.get(currIndeks);
+        }
+        return null;
     }
 }
