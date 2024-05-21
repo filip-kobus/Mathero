5cb2a2575230e362aff9695a2e7969d5435bc03d
filipkobus <kobusfilip@outlook.com>
2024-05-21 19:38:52 +0200

    Dodanie ramki do input fielda w rozwiązywaniu zadań

--- src/main/java/Gui/NumberInputField.java
+++ src/main/java/Gui/NumberInputField.java
@@ -9,10 +9,11 @@ public class NumberInputField extends JTextField {
     public NumberInputField(int maxLength) {
         super();
         setDocument(createNumberDocument(maxLength));
-        setPreferredSize(new Dimension(maxLength*38, 60));
+        setPreferredSize(new Dimension(maxLength * 38, 60));
         setBackground(new Color(52, 24, 73));
         setForeground(Color.WHITE);
-        setBorder(javax.swing.BorderFactory.createEmptyBorder());
+        // Set a border only on the south side (bottom)
+        setBorder(BorderFactory.createMatteBorder(0, 0, 1, 0, Color.WHITE));
         setFont(new Font("Segoe UI Black", Font.BOLD, 60));
         setHorizontalAlignment(JTextField.CENTER);
         setCaretColor(Color.WHITE);
